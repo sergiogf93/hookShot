fce6d72d9acca570ebc7717b6c3d11648fc7a9ac
sergiogf93 <sergiogf93@gmail.com>
2017-06-19 15:29:58 +0200

    Map random values set

--- java/com/htss/hookshot/map/Map.java
+++ java/com/htss/hookshot/map/Map.java
@@ -108,7 +108,6 @@ public class Map {
         }
         if (MyActivity.canvas.myActivity.level > 0) {
             double r = addingRandom.nextDouble();
-            r = 0.2;
             if (r < 0.4) {
                 addPassageDoor(2);
                 addExitDoor(addingRandom, MAX_BUTTONS);
